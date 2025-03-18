f354d5b30b4f6d5023fadf8cd28ba06bdbcb6ad8
U~Iriamuzu <daviswill264@gmail.com>
2025-03-18 20:49:29 +0100

    for checks

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,11 @@
+// "builds": [
+//   {
+//     "src": "index.js",
+//     "use": "@vercel/node"
+//   }
+// ],
 // "routes": [
 //   { "src": "/(.*)", "dest": "/index.js" }
 // ],
+// "outputDirectory": "dist",
+// "public": "dist",
