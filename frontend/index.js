ddf95abd966bdc071bb88cdd6490950a405f8ff1
U~Iriamuzu <daviswill264@gmail.com>
2025-03-19 16:59:37 +0100

    changes completed

--- frontend/index.js
+++ frontend/index.js
@@ -9,3 +9,6 @@
 // ],
 // "outputDirectory": "dist",
 // "public": "dist",
+
+// "build": "npx browserify ./index.js -g [ envify --NODE_ENV production ] -g uglifyify | terser --compress --mangle > ./dist/bundle.js",
+// 
